ab249581d12a0805ebc58f9a48e57223d4dc4aa4
chartma92 <52803019+chartma92@users.noreply.github.com>
2019-07-17 15:46:31 -0400

    Day 3 finish

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,7 @@
+import './styles.css';
+import { ready } from './ready';
+import { runApp } from './app';
 
-const nameInput = document.getElementById('name') as HTMLInputElement;
-const doitButton = document.getElementById('doit') as HTMLInputElement;
-const messageOutput = document.getElementById('message');
+ready(() => console.log('Ready to Party!'))
 
-doitButton.addEventListener('click', function () {
-    const enteredname = nameInput.value;
-    messageOutput.innerText = enteredname.toUpperCase();
-})
+ready(runApp)
